dda420db57840be4fae3a4cd8a77c1917b258f1a
philh4420 <64500979+philh4420@users.noreply.github.com>
2023-05-14 11:36:05 +0100

    done a complete restyle of everything

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-// App.js
-
 import React, { useState, useEffect } from 'react';
 import Movie from '../src/Movie';
 import './App.css';
 
 const API_KEY = '66960d3b38e7771e0fbadad49521d15f';
-const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_POPULAR_URL = `${API_BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+const API_SEARCH_URL = `${API_BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=`;
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    fetch(API_URL)
+    fetch(API_POPULAR_URL)
       .then(response => response.json())
       .then(data => setMovies(data.results))
       .catch(error => console.log(error));
   }, []);
 
+  const handleSearchInput = e => {
+    setSearchQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchQuery === '') {
+      fetch(API_POPULAR_URL)
+        .then(response => response.json())
+        .then(data => setMovies(data.results))
+        .catch(error => console.log(error));
+    } else {
+      fetch(API_SEARCH_URL + searchQuery)
+        .then(response => response.json())
+        .then(data => setMovies(data.results))
+        .catch(error => console.log(error));
+    }
+  }, [searchQuery]);
+
   return (
-    <div className="movie-container">
-      {movies.map(movie => (
-        <Movie key={movie.id} {...movie} />
-      ))}
+    <div>
+      <div className="search-container">
+        <input className="search-input"
+          type="text"
+          placeholder="Search for a movie..."
+          value={searchQuery}
+          onChange={handleSearchInput}
+        />
+      </div>
+      <div className="movie-container">
+        {movies.map(movie => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,22 +1,42 @@
-// Movie.js
-
 import React from 'react';
-import '../src/App.css';
+import PropTypes from 'prop-types';
 
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
+const Movie = ({ title, poster_path, overview, vote_average, release_date }) => {
+  // Define default poster image URL in case poster_path is not provided
+  const posterUrl = poster_path
+    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+    : 'https://via.placeholder.com/500x750.png?text=No+Poster+Available';
 
-const Movie = ({ title, poster_path, overview, vote_average }) => (
-  <div className="movie">
-    <img src={`${IMAGE_BASE_URL}${poster_path}`} alt={title} />
-    <div className="movie-info">
-      <h3>{title}</h3>
-      <span className="rating">{vote_average}</span>
+  // Convert vote_average to a 5-star rating scale
+  const rating = Math.round(vote_average / 2);
+
+  // Format release_date to display only the year
+  const year = release_date ? release_date.slice(0, 4) : '';
+
+  return (
+    <div className="movie">
+      <div className="movie-poster-container">
+        <img className="movie-poster" src={posterUrl} alt={title} />
+      </div>
+      <div className="movie-info">
+        <h3 className="movie-title">{title}</h3>
+        <div className="movie-details">
+          <span className="movie-year">{year}</span>
+          <span className={`movie-rating movie-rating-${rating}`}>{rating}</span>
+        </div>
+        <p className="movie-overview">{overview}</p>
+      </div>
     </div>
-    <div className="movie-overview">
-      <h2>Overview:</h2>
-      <p>{overview}</p>
-    </div>
-  </div>
-);
+  );
+};
+
+// Define prop types for Movie component
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster_path: PropTypes.string,
+  overview: PropTypes.string.isRequired,
+  vote_average: PropTypes.number.isRequired,
+  release_date: PropTypes.string
+};
 
 export default Movie;
