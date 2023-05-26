b59e72688f7f3af66be1e2bdb210676b6109f10c
philh4420 <64500979+philh4420@users.noreply.github.com>
2023-05-26 13:31:49 +0100

    search function + preconnect or dns-prefetch

--- src/App.js
+++ src/App.js
@@ -4,24 +4,19 @@ import './App.css';
 
 const API_KEY = '66960d3b38e7771e0fbadad49521d15f';
 const API_BASE_URL = 'https://api.themoviedb.org/3';
-const API_MOVIE_POPULAR_URL = `${API_BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
-const API_TV_POPULAR_URL = `${API_BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
-const API_SEARCH_MOVIE_URL = `${API_BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=`;
-const API_SEARCH_TV_URL = `${API_BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&query=`;
+const API_POPULAR_URL = `${API_BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+const API_SEARCH_URL = `${API_BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=`;
 
 function App() {
-  const [mediaType, setMediaType] = useState('movie');
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetchPopularMedia();
-  }, [mediaType]);
+  }, []);
 
   const fetchPopularMedia = () => {
-    const apiUrl = mediaType === 'movie' ? API_MOVIE_POPULAR_URL : API_TV_POPULAR_URL;
-
-    fetch(apiUrl)
+    fetch(API_POPULAR_URL)
       .then(response => response.json())
       .then(data => setMovies(data.results))
       .catch(error => console.log(error));
@@ -31,22 +26,22 @@ function App() {
     setSearchQuery(e.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchMovies = () => {
     if (searchQuery === '') {
       fetchPopularMedia();
     } else {
-      const searchUrl = mediaType === 'movie' ? API_SEARCH_MOVIE_URL : API_SEARCH_TV_URL;
-
-      fetch(searchUrl + searchQuery)
+      fetch(API_SEARCH_URL + searchQuery)
         .then(response => response.json())
         .then(data => setMovies(data.results))
         .catch(error => console.log(error));
     }
   };
 
-  const handleMediaTypeToggle = () => {
-    setMediaType(prevMediaType => (prevMediaType === 'movie' ? 'tv' : 'movie'));
-    setSearchQuery('');
+  const handleSearchTVSeries = () => {
+    fetch(`${API_BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
+      .then(response => response.json())
+      .then(data => setMovies(data.results))
+      .catch(error => console.log(error));
   };
 
   return (
@@ -55,15 +50,21 @@ function App() {
         <input
           className="search-input"
           type="text"
-          placeholder={`Search for a ${mediaType === 'movie' ? 'movie' : 'TV series'}...`}
+          placeholder="Search for a movie..."
           value={searchQuery}
           onChange={handleSearchInput}
         />
-        <button className="search-button" onClick={handleSearchSubmit}>
-          Search
+        <button
+          className="search-button"
+          onClick={handleSearchMovies}
+        >
+          Search Movies
         </button>
-        <button className="media-type-toggle" onClick={handleMediaTypeToggle}>
-          {mediaType === 'movie' ? 'TV Series' : 'Movies'}
+        <button
+          className="search-button"
+          onClick={handleSearchTVSeries}
+        >
+          Search TV Series
         </button>
       </div>
       <div className="movie-container">
